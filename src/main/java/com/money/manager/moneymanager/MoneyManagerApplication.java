f852e39e9d30cf6dc78c6a6ed9e9ca860a6bf838
fcsouza <fcsouza18@hotmail.com>
2021-05-29 17:00:38 -0300

    init project

---
+++ src/main/java/com/money/manager/moneymanager/MoneyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.money.manager.moneymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/money/manager/moneymanager/MoneyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.money.manager.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
