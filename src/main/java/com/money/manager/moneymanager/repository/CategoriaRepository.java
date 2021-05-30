c91d130909c95e5a3ac7e5b1f24120b6b0cf7d8d
fcsouza <fcsouza18@hotmail.com>
2021-05-30 19:04:07 -0300

    include get all

---
+++ src/main/java/com/money/manager/moneymanager/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.money.manager.moneymanager.repository;
+
+
+import com.money.manager.moneymanager.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
---
+++ src/main/java/com/money/manager/moneymanager/resource/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.money.manager.moneymanager.resource;
+
+
+import com.money.manager.moneymanager.model.Categoria;
+import com.money.manager.moneymanager.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar(){
+		return categoriaRepository.findAll();
+	}
+
+}
