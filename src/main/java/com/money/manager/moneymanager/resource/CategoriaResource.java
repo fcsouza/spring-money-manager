d89ebfb3796ac468bd3e2db2392da5d744eb604e
fcsouza <fcsouza18@hotmail.com>
2021-06-01 20:50:07 -0300

    include post request

--- src/main/java/com/money/manager/moneymanager/resource/CategoriaResource.java
+++ src/main/java/com/money/manager/moneymanager/resource/CategoriaResource.java
@@ -4,11 +4,16 @@ package com.money.manager.moneymanager.resource;
 import com.money.manager.moneymanager.model.Categoria;
 import com.money.manager.moneymanager.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequestUri;
 
 @RestController
 @RequestMapping("/categorias")
@@ -20,6 +25,18 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequestUri()
+				.path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 
 }
